8922368ff02b4eca18612fc9bf0b21b23f354ed2
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-06-17 11:58:11 +0200

    new exercise: 04-vector-into-map

--- exercises/04-vector-into-map/tests/suite.cpp
+++ exercises/04-vector-into-map/tests/suite.cpp
@@ -17,3 +17,19 @@ TEST(vector_into_map_suite, basic)
     ASSERT_EQ(pair_map["zwei"], 2);
     ASSERT_EQ(pair_map["drei"], 3);
 }
+
+TEST(vector_into_map_suite, no_overwrites)
+{
+    using my_pair = std::pair<std::string, int>;
+    using my_pair_array = std::vector<my_pair>;
+    my_pair_array pair_array{{"eins", 1}, {"zwei", 2}, {"drei", 3}, {"eins", 1000}};
+
+    using my_pair_map = std::map<std::string, int>;
+
+    my_pair_map pair_map = vector_into_map(pair_array);
+
+    ASSERT_EQ(pair_map.size(), 3);
+    ASSERT_EQ(pair_map["eins"], 1);
+    ASSERT_EQ(pair_map["zwei"], 2);
+    ASSERT_EQ(pair_map["drei"], 3);
+}

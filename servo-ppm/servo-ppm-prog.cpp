4e74cfb02a817974c086a9d62fb8aabbd5dc768b
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-10-08 13:11:25 +0200

    add subdir for each group

---
+++ servo-ppm/servo-ppm-prog.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    (void)argc; // unused
+    (void)argv; // unused
+
+    std::cout << "hier geschieht ein wunder" << std::endl;
+
+    return 0;
+}

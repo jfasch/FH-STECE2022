e8371a65d2a73ec3c3ce4d996a08ec3d450cb93c
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-10-29 12:58:42 +0100

    point.cpp complete (pooh)

--- livecoding/point.cpp
+++ livecoding/point.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
 using namespace std;
 
-struct point
-{
-    int x;
-    int y;
-};
+// struct Gyro
+// {
+//     int fd;
+// };
 
-void point_print(point p)
+class point
 {
-    cout << '(' << p.x << ',' << p.y << ")\n";
-}
+public:
+    point()
+    : _x(0), _y(0) // initializer list
+    {}
+
+    point(int x, int y)
+    : _x(x), _y(y) // initializer list
+    {}
+
+    point move(point vec)
+    {
+        int x = _x + vec._x;
+        int y = _y + vec._y;
+        return point(x, y); // temporary object
+    }
+
+    void print()
+    {
+        cout << '(' << _x << ',' << _y << ")\n";
+    }
+
+private:
+    const int _x;
+    const int _y;
+};
 
 int main()
 {
-    point p;
-    point_print(p);
-    
+    point p(1, 2);
+    point vec(3, 1);
+    point moved = p.move(vec);
+    cout << "original p: ";
+    p.print();
+    cout << "moved: ";
+    moved.print();
+
+    point p_default;
+    p_default.print();
+
     return 0;
 }

42bbffd66a15aaae175ae07f8dd999f73805261c
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-11-22 11:37:58 +0100

    sysfs-motor: pull impl into .cpp

--- toolcase/base/sysfs-motor.h
+++ toolcase/base/sysfs-motor.h
@@ -11,52 +11,11 @@ public:
     :_forward(forward), _backward(backward), _speed(speed), _was_speed_set(false)
     {}
 
-    void set_speed(int speed) {
-
-        if(speed > 100)
-        {
-            speed = 100;
-        }
-
-        if(speed < -100)
-        {
-            speed = -100;
-        }
-
-        if (speed > 0)
-        {
-            _forward.set_state(true);
-            _backward.set_state(false);
-            _speed.set_duty_cycle(_speed.period() / 100 * std::abs(speed));  
-            _was_speed_set = true;  
-        }
-
-        else if(speed < 0)
-        {
-            _forward.set_state(false);
-            _backward.set_state(true);
-            _speed.set_duty_cycle(_speed.period() / 100 * std::abs(speed));
-            _was_speed_set = true;
-        }
-
-        else if(speed == 0 && _was_speed_set == false)
-        {
-            _forward.set_state(false);
-            _backward.set_state(false);
-            _speed.set_duty_cycle(_speed.period() / 100 * std::abs(speed));
-        }
-
-        else
-        {
-            _speed.set_duty_cycle(_speed.period() / 100 * std::abs(speed));
-        }
-        
-    }
+    void set_speed(int speed);
 
 private:
-SysFS_GPIO_Pin _forward;
-SysFS_GPIO_Pin _backward;
-SysFS_PWM_Pin _speed;
-bool _was_speed_set;
+    SysFS_GPIO_Pin _forward;
+    SysFS_GPIO_Pin _backward;
+    SysFS_PWM_Pin _speed;
+    bool _was_speed_set;
 };
-

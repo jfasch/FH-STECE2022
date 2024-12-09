bc57fdeea877914f041b628a8e15b3cb60546071
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-12-09 12:18:09 +0100

    docs: 2024-12-09

--- bin/crazy-car-config.h
+++ bin/crazy-car-config.h
@@ -2,31 +2,19 @@
 
 #define CRAZY_CAR_MQ_NAME "/crazy-car"
 
-// struct pwm_config
-// {
-//     int chip;
-//     int pin;
-//     int hw_gpio; // from pi pinout
+// depends on /boot/config.txt
+// #define CRAZY_CAR_MOTOR_SPEED_PWM_DIR "/sys/class/pwm/pwmchip{0,1}/pwm2"
+// #define CRAZY_CAR_MOTOR_FORWARD_PIN "/sys/class/gpio/gpio8"
+// ...
 
-// };
 
-// struct gpio_config
-// {
-//     int gpio;   // from pi pinout
-// };
+// sensor address conflict
+// - gyro is at 0x28 by default
+// - tof is at 0x29 by default BUT WE HAVE THREE OF THEM
 
-// pwm_config servo_pwm_config = {  // -> /sys/class/pwm/pwmchip0/pwm0
-//     .chip = 0, 
-//     .pin = 0,
-//     .hw_gpio = 18,
-// };
-// pwm_config motor_pwm_config = {
-//     // ...
-// };
-// // motor_pwm = pwm(pwmchip 0, pwmpin 1, gpio 19); // -> /sys/class/pwm/pwmchip0/pwm1
+// - crazy-car-init: time-of-flight sensor address config
 
-// gpio_config motor_forward = {
-//     .gpio = 7,
-// };
-// // motor_backward ...
-
+//      - order 3 breakouts with *six* pins (including XSHUT (hardware suspend))
+//      - connect XSHUT to dedicated GPIOS
+//      - hold all in hardware suspend
+//      - release one after the other, configuring addresses in software as we go

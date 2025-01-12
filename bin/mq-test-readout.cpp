3cb1756193e2db39ef64a90a92897ce8aa3834c6
Lorenz Buchinger <lorenz.buchinger@edu.fh-joanneum.at>
2025-01-12 22:13:04 +0100

    crazy-car-shell.cpp and test programm (mq-test-readout) implemented and tested

---
+++ bin/mq-test-readout.cpp
@@ -0,0 +1,42 @@
+#include "crazy-car-config.h"
+#include "../toolcase/ipc/crazy-car-protocol.h"
+
+#include <mqueue.h>
+#include <fcntl.h>
+#include <cstdio>
+#include <cassert>
+#include <iostream>
+
+
+int main()
+{
+    mqd_t message_queue = mq_open(CRAZY_CAR_MQ_NAME, O_RDONLY); 
+    if (message_queue == -1) {
+        perror("mq_open");
+        return 1;
+    }
+    while (true) {
+        CrazyCarMessage cur_msg;
+        unsigned int priority;
+
+        ssize_t n_bytes_read = mq_receive(message_queue, (char*)&cur_msg, sizeof(cur_msg), &priority);
+        if (n_bytes_read == -1) {
+            perror("mq_receive");
+            return 2;
+        }
+        assert(n_bytes_read == sizeof(cur_msg));
+
+        switch (cur_msg.command) {
+            case MOTOR_SET_FRACTION_SPEED_PERCENT:
+                std::cout << "Set motor speed to: " << cur_msg.value << std::endl;
+                break;
+            case SERVO_SET_ANGLE_PERCENT:
+                std::cout << "Set Servo to: " << cur_msg.value << std::endl;
+                break;
+            default:
+                assert("bad command");
+                break;
+        }
+    }
+    return 0;
+}
